16962c454b8d63080274f3e7229a945bc8a5e081
Kevin Hartley <nivek1385@yeltrahnivek.com>
2017-08-10 00:31:41 -0400

    Updated phasetofb.sh

--- phasetofb.sh
+++ phasetofb.sh
@@ -2,7 +2,7 @@
 source ~/.bash_profile
 #builtin cd ~
 weather Moon.png >> moon.png
-illum=$(phase nowttr | grep illuminated)
+illum=$(phase nowttr | grep illumination)
 echo $illum
 python ~/bin/send.py "nivek1385@yeltrahnivek.com" "Moon Phase Script" "$illum" "moon.png"
 rm moon.png
