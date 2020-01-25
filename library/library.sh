92b57f052de74685d50e0cdc087dc1eb55f67666
Kevin Hartley <nivek1385@yeltrahnivek.com>
2020-01-25 17:23:00 -0500

    Adding library folder and library.sh

---
+++ library/library.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+AbortScript() {
+  Warn "ABORTING SCRIPT AT LINE NUMBER $1"
+  exit 2
+}
+
+logdtstamp() {
+  date +'%Y-%m-%d %H:%M:%S'
+}
+
+Error() {
+  echo "*****ERROR: $@ *****"
+}
+
+logger() {
+  tee -a $1-$dtstamp.log
+}
+
+pause() {
+  read -p "$*"
+}
+
+rootCheck() {
+  curUser=$(whoami)
+  if [ "$curUser" != "root" ]; then
+    Error "Sorry, but you must be root to execute this script."
+	exit 1
+  fi
+  return
+}
+
+Warn() {
+  echo "*****WARNING: $@ *****"
+}
+
+Debug() {
+  echo "Starting debug mode."
+  set -x
+}
+
+Undebug() {
+  set +x
+  echo "Stopped debug mode."
+}
+
+logUsage() {
+  #Usage: logUsage "$*"
+  logdir=$LOGDIR
+  mkdir -p $logdir
+  outinfo "Logging usage of script..."
+  echo "$(logdtstamp) - $(whoami)@$HOSTNAME $0 $*" >> $logdir/logUsage.log
+}
+
+#Output for logging
+### verbosity levels
+silent_lvl=0
+crit_lvl=1
+err_lvl=2
+warn_lvl=3
+notify_lvl=4
+info_lvl=5
+debug_lvl=6
+##Colors:
+colblk='\033[0;30m' # Black - Regular
+colred='\033[0;31m' # Red
+colgrn='\033[0;32m' # Green
+colylw='\033[0;33m' # Yellow
+colwht='\033[0;34m' # White 
+colpur='\033[0;35m' # Purple
+colrst='\033[0m'    # Text Reset
+## outsilent prints output even in silent mode
+function outsilent () { verb_lvl=$silent_lvl outlog "$@" ;}
+function outnotify () { verb_lvl=$notify_lvl outlog "$@" ;}
+function outok ()    { verb_lvl=$notify_lvl outlog "SUCCESS - $@" ;}
+function outwarn ()  { verb_lvl=$warn_lvl outlog "${colylw}WARNING${colrst} - $@" ;}
+function outinfo ()  { verb_lvl=$info_lvl outlog "${colwht}INFO${colrst} ---- $@" ;}
+function outdebug () { verb_lvl=$debug_lvl outlog "${colgrn}DEBUG${colrst} --- $@" ;}
+function outerror () { verb_lvl=$err_lvl outlog "${colred}ERROR${colrst} --- $@" ;}
+function outcrit ()  { verb_lvl=$crit_lvl outlog "${colpur}FATAL${colrst} --- $@" ;}
+function outdumpvar () { for var in $@ ; do outdebug "$var=${!var}" ; done }
+function outlog() {
+        if [ $verbosity -ge $verb_lvl ]; then
+                datestring=`date +"%Y-%m-%d %H:%M:%S"`
+                echo -e "$datestring - $@"
+        fi
+}
+
+#Logging setup
+export LOGDIR=~/logs
+export DATE=`date +"%Y%m%d"`
+export DATETIME=`date +"%Y%m%d_%H%M%S"`
+ 
+ScriptName=`basename $0`
+Job=`basename $0 .sh`"$*"
+JobClass=`basename $0 .sh`
+ 
+startlog() {
+        if [ $NO_JOB_LOGGING ] ; then
+                outinfo "Not logging to a logfile because -Z option specified." #(*)
+        else
+                [[ -d $LOGDIR/$JobClass ]] || mkdir -p $LOGDIR/$JobClass
+                Pipe=${LOGDIR}/$JobClass/${Job}_${DATETIME}.pipe
+                mkfifo -m 700 $Pipe
+                LOGFILE=${LOGDIR}/$JobClass/${Job}_${DATETIME}.log
+                exec 3>&1
+                tee ${LOGFILE} <$Pipe >&3 &
+                teepid=$!
+                exec 1>$Pipe
+                PIPE_OPENED=1
+                outnotify Logging to $LOGFILE  # (*)
+                [ $SUDO_USER ] && outnotify "Sudo user: $SUDO_USER" #(*)
+        fi
+}
+ 
+stoplog() {
+        if [ ${PIPE_OPENED} ] ; then
+                exec 1<&3
+                sleep 0.2
+                ps --pid $teepid >/dev/null
+                if [ $? -eq 0 ] ; then
+                        # a wait $teepid whould be better but some
+                        # commands leave file descriptors open
+                        sleep 1
+                        kill  $teepid
+                fi
+                rm $Pipe
+                unset PIPE_OPENED
+        fi
+}
