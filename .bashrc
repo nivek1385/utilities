625c08b05d058bfcfc01cc756fd21591eb1edd48
Kevin Hartley <nivek1385@yeltrahnivek.com>
2020-01-27 16:26:51 -0500

    Updated .bash_aliases and .bashrc; added colors.sh

--- .bashrc
+++ .bashrc
@@ -16,8 +16,17 @@ HISTCONTROL=ignoreboth
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+HISTIGNORE='&:[ ]*:exit:ls:bg:fg:jobs:history:clear:pwd'
+
+#Add color defs
+if [ -f ~/bin/colors.sh ]; then
+  . ~/bin/colors.sh
+fi
+#Set Prompt_CMD
+PROMPT_COMMAND='if [[ $? -eq 0 ]]; then echo -e "$green$(~/bin/latindate)$RESET"; else echo -e "$red$(~/bin/latindate)$RESET"; fi'
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
@@ -90,15 +99,6 @@ fi
 
 # colored GCC warnings and errors
 #export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # Alias definitions.
 # You may want to put all your additions into a separate file like
---
+++ colors.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+alias echo='echo -e'
+
+#General Text Attributes
+reset='\e[0m'         #Reset all attributes
+RESET='\e[0m'         #Reset all attributes
+default='\e[39m'      #Default foreground
+DEFAULT='\e[40m'      #Default background
+
+bold='\e[1m'          #Bold
+faint='\e[2m'         #Faint
+standout='\e[3m'      #Standout
+underscore='\e[4m'    #Underscore
+blink='\e[5m'         #Blink
+reverse='\e[6m'       #Reverse
+hidden='\e[7m'        #Hidden
+
+nostandout='\e[23m'   #No standout
+nounderscore='\e[24m' #No underscore
+noblink='\e[25m'      #No blink
+noreverse='\e[26m'    #No reverse
+
+#Foreground colors
+black='\e[30m'
+red='\e[31m'
+green='\e[32m'
+yellow='\e[33m'
+blue='\e[34m'
+magenta='\e[35m'
+cyan='\e[36m'
+white='\e[37m'
+dgray='\e[90m'
+lred='\e[91m'
+lgreen='\e[92m'
+lyellow='\e[93m'
+lblue='\e[94m'
+lmagenta='\e[95m'
+lcyan='\e[96m'
+lgray='\e[97m'
+dgrey='\e[90m'
+lgrey='\e[97m'
+
+#Background colors
+BLACK='\e[40m'
+RED='\e[41m'
+GREEN='\e[42m'
+YELLOW='\e[43m'
+BLUE='\e[44m'
+MAGENTA='\e[45m'
+CYAN='\e[46m'
+WHITE='\e[47m'
+DGRAY='\e[100m'
+LRED='\e[101m'
+LGREEN='\e[102m'
+LYELLOW='\e[103m'
+LBLUE='\e[104m'
+LMAGENTA='\e[105m'
+LCYAN='\e[106m'
+LGRAY='\e[107m'
+DGREY='\e[100m'
+LGREY='\e[107m'
+
